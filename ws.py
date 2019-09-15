8e1a9729cf845557a4973ddfe618132528a74898
pakashabhinav7 <Prakash@iastate.edu>
2019-09-15 00:59:42 -0500

    added function crawler_collector

--- ws.py
+++ ws.py
@@ -1,21 +1,22 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-
-#TODO Create function for reader(to copy data from the url/list of urls)
-#TODO Create function for writing results to file/db
-
 my_url = 'https://www.newegg.com/Video-Cards-Devices/Category/ID-38'
-uClient = uReq(my_url)  # opens connection
-page_html = uClient.read()  # reads the page
-uClient.close()  # disconnect client
 
-page_soup = soup(page_html, "html.parser")  # html parsing
 
-#TODO grab each product and test out the traversal for each product
+# TODO Create function for reader(to copy data from the url/list of urls)
+# TODO Create function for writing results to file/db
+
+def crawler_collector(url_to_crawl):
+    u_client = uReq(url_to_crawl)  # opens connection
+    page_html = u_client.read()  # reads the page
+    u_client.close()  # disconnect client
+    page_soup = soup(page_html, "html.parser")  # html parsing
+
+# TODO grab each product and test out the traversal for each product
 containers = page_soup.find_all("ul", {"class": "item-features"})
 
-#TODO implement try except for boundary cases
+# TODO implement try except for boundary cases
 for container in containers:
     # brand = container.div.div.a.img["title"]
     print(container.li)
