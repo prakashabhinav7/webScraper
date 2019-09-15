0c2be3f93fcc8f531236040bae2f8b9b169e98b0
pakashabhinav7 <Prakash@iastate.edu>
2019-09-15 16:59:33 -0500

    Added matplotlib to visualise the price
    Signed-off-by: pakashabhinav7 <Prakash@iastate.edu>
    #TODO clean up the dataframe as it is only displaying the prices as of now

--- ws.py
+++ ws.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import matplotlib.pyplot as plt
 
 my_url = 'https://www.newegg.com/p/pl?Submit=StoreIM&Depa=80&PageSize=96'
 
@@ -16,13 +17,18 @@ def price_tracker_df(url_to_track):
         title = str(item.find_all('a', class_='item-title')[0].text).replace("\r\n ", "")
         price = str(item.find_all('li', class_='price-current')[0].strong.text).replace("\r\n ", "")
         shipping = str(item.find_all('li', class_="price-ship")[0].text).replace("\r\n ", "")
-        row = (('Title', title), ('Shipping', shipping.strip()), ('Price', price))
+        row = (('Title', title), ('Shipping', shipping.strip()), ('Price', price.replace(",", "")))
         row_dict.update(row)  # New dictionary updated onto the old one
         dict_list.append(row_dict)  # Append the new dictionary to the list of dictionaries
     price_tracker = pd.DataFrame(dict_list, columns=['Title', 'Shipping', 'Price'])  # Write the list of dictionaries
     # to a pandas dataframe
 
-    return price_tracker
+    return price_tracker['Price'].astype(str).astype(int)
 
 
-print(price_tracker_df(my_url))
+price_tracker = price_tracker_df(my_url)
+plt.plot(price_tracker)
+plt.xlabel('Title')
+plt.ylabel('Price')
+plt.show()
+# price_tracker.plot.bar(x='Title', y='Price')
